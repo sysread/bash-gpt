6951aab32786c0375a9db706e48742a7feb8275e
Joe Williams <jwilliams@joinpapa.com>
2024-01-05 11:34:51 -0500

    added better setup messaging

--- flake.nix
+++ flake.nix
@@ -11,54 +11,71 @@
       let
 
         pkgs = nixpkgs.legacyPackages.${system};
+        env = import ./env.nix;
 
         nativeBuildInputs = with pkgs; [ ];
-        buildInputs = with pkgs; [];
+        buildInputs = with pkgs; [ ];
       in {
+
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "bash-gpt";
           version = "0.0.0";
           src = ./.;
           inherit nativeBuildInputs buildInputs;
 
-            installPhase = ''
-              # Custom installation commands
-              mkdir -p $out/bin
-              #cp gpt $out/bin 
-              cp gpt $out/bin && chmod +x $out/bin/gpt
-              cp openai $out/bin && chmod +x $out/bin/openai
-              cp chat $out/bin && chmod +x $out/bin/chat
-              cp chat-beta $out/bin && chmod +x $out/bin/chat-beta
-              cp code $out/bin && chmod +x $out/bin/code
-              cp cmd $out/bin && chmod +x $out/bin/cmd
-              cp tester $out/bin && chmod +x $out/bin/tester
-            '';
+          installPhase = ''
+            # Custom installation commands
+            mkdir -p $out/bin
+            #cp gpt $out/bin 
+            cp gpt $out/bin && chmod +x $out/bin/gpt
+            cp openai $out/bin && chmod +x $out/bin/openai
+            cp chat $out/bin && chmod +x $out/bin/chat
+            cp chat-beta $out/bin && chmod +x $out/bin/chat-beta
+            cp code $out/bin && chmod +x $out/bin/code
+            cp cmd $out/bin && chmod +x $out/bin/cmd
+            cp tester $out/bin && chmod +x $out/bin/tester
+          '';
         };
         devShells.default = pkgs.mkShell {
           inherit nativeBuildInputs buildInputs;
           src = ./.;
-
-            installPhase = ''
-              # Custom installation commands
-              mkdir -p $out/bin
-              #cp gpt $out/bin 
-              cp gpt $out/bin && chmod +x $out/bin/gpt
-              cp openai $out/bin && chmod +x $out/bin/openai
-              cp chat $out/bin && chmod +x $out/bin/chat
-              cp chat-beta $out/bin && chmod +x $out/bin/chat-beta
-              cp code $out/bin && chmod +x $out/bin/code
-              cp cmd $out/bin && chmod +x $out/bin/cmd
-              cp tester $out/bin && chmod +x $out/bin/tester
-            '';
+          installPhase = ''
+            # Custom installation commands
+            mkdir -p $out/bin
+            #cp gpt $out/bin 
+            cp gpt $out/bin && chmod +x $out/bin/gpt
+            cp openai $out/bin && chmod +x $out/bin/openai
+            cp chat $out/bin && chmod +x $out/bin/chat
+            cp chat-beta $out/bin && chmod +x $out/bin/chat-beta
+            cp code $out/bin && chmod +x $out/bin/code
+            cp cmd $out/bin && chmod +x $out/bin/cmd
+            cp tester $out/bin && chmod +x $out/bin/tester
+          '';
 
           shellHook = ''
+            FETCH_API_KEY_MESSAGE="Please go to https://beta.openai.com/account/api-keys then set the OPEN_API_KEY environment variable"
+            SET_API_KEY_MESSAGE="Please go to .env and update your environment variables."
+            if source .env 
+            then
+              echo "Env File Already Generated"
+            else
+              echo "OPENAI_API_KEY=\"\" 
+          OPENAI_API_MODEL=\"gpt-3.5-turbo-16k\" " >> .env
+
+              echo $FETCH_API_KEY_MESSAGE
+              echo $SET_API_KEY_MESSAGE
+              exit 1
+            fi
+
+            if [[ "$OPENAI_API_KEY" == "" ]]; then
+              echo $FETCH_API_KEY_MESSAGE
+              echo $SET_API_KEY_MESSAGE
+              exit 1
+            fi
+
             echo "Welcome to Bash GPT!"
             echo "Available commands: chat | chat-beta | code | cmd | tester"
-            if [ $OPENAI_API_KEY == "" ]; then
-              echo "Please go to https://beta.openai.com/account/api-keys then set the OPEN_API_KEY environment variable"
-            fi
             exec $SHELL
-
           '';
 
         };
